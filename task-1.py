114a436be9e85aefd7b5ab0696fe5e101ebded29
Rohan19005742 <60757115+Rohan19005742@users.noreply.github.com>
2024-12-16 15:28:25 +0000

    answer

--- task-1.py
+++ task-1.py
@@ -1,7 +1,7 @@
 def compound_interest(principal, rate, time):
     Amount = principal
     for i in range(time):
-        Amount = Amount * (1 + rate/10)
+        Amount = Amount * (1 + rate/100)
     CI = Amount - principal
     print("Compound interest is", CI)
 compound_interest(1200, 10, 2)
